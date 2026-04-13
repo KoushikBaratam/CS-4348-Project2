f8f83ab66a8ac0e74453520233abd7b391741355
KoushikBaratam <appibaratam@gmail.com>
2026-04-13 01:50:54 -0500

    implemented the thread creation and the startup logic

--- BankSimulation.java
+++ BankSimulation.java
@@ -9,5 +9,42 @@ public class BankSimulation {
         Teller[] tellers = new Teller[3];
         Customer[] customers = new Customer[50];
 
+        // create and start teller threads
+        for (int i = 0; i < 3; i++) {
+            tellers[i] = new Teller(i, bank);
+            tellers[i].start();
+        }
+
+        // create and start customer threads
+        for (int i = 0; i < 50; i++) {
+            customers[i] = new Customer(i, bank);
+            customers[i].start();
+        }
+
+        // wait for customers to finish
+        for (int i = 0; i < 50; i++) {
+            try {
+                customers[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // stop tellers
+        for (int i = 0; i < 3; i++) {
+            tellers[i].stopTeller();
+        }
+
+        // wait for tellers
+        for (int i = 0; i < 3; i++) {
+            try {
+                tellers[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Simulation finished.");
+
     }
 }
