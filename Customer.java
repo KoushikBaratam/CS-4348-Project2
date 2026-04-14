b110cb2ddc5ac95addd5526909c648f27d0c05a2
KoushikBaratam <appibaratam@gmail.com>
2026-04-13 19:50:39 -0500

    implemented customer arrival and the teller selection behavior

--- Customer.java
+++ Customer.java
@@ -21,5 +21,70 @@ public class Customer extends Thread {
 
     public void run() {
 
+        try {
+            // random transaction choice
+            String transaction =
+                    rand.nextBoolean() ? "Deposit" : "Withdraw";
+
+            bank.log("Customer " + id + " []: wants to perform a " +
+                    transaction.toLowerCase() + " transaction");
+
+            // wait before arriving
+            Thread.sleep(rand.nextInt(101));
+
+            bank.log("Customer " + id + " []: going to bank.");
+
+            // door allows only two at a time
+            bank.door.acquire();
+
+            bank.log("Customer " + id + " []: entering bank.");
+
+            bank.door.release();
+
+            bank.log("Customer " + id + " []: getting in line.");
+
+            // wait for teller
+            bank.availableTellers.acquire();
+
+            Teller teller;
+
+            synchronized (bank) {
+                teller = bank.getReadyTeller();
+            }
+
+            bank.log("Customer " + id + " []: selecting a teller.");
+
+            bank.log("Customer " + id + " [Teller " +
+                    teller.id + "]: selects teller");
+
+            bank.log("Customer " + id + " [Teller " +
+                    teller.id + "]: introduces itself");
+
+            // assign to teller
+            teller.assignCustomer(this, transaction);
+
+            bank.log("Customer " + id + " [Teller " +
+                    teller.id + "]: asks for " +
+                    transaction.toLowerCase() + " transaction");
+
+            // wait for teller to finish
+            teller.finished.acquire();
+
+            bank.log("Customer " + id + " [Teller " +
+                    teller.id + "]: leaves teller");
+
+            bank.log("Customer " + id + " []: goes to door");
+
+            bank.door.acquire();
+
+            bank.log("Customer " + id + " []: leaves the bank");
+
+            bank.door.release();
+
+            bank.customerServed();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
